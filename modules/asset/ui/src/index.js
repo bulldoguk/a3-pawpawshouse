e7c457d099259229e213b1b943140e7f44e35293
bulldoguk <57550309+bulldoguk@users.noreply.github.com>
2022-01-18 06:27:07 -0600

    search and hamburger menues working

--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -1,4 +1,12 @@
 export default () => {
   // Your own project level JS may go here
-  console.log('Hello World');
+  console.log("Hello World");
+
+  // To drive hamburger menu
+  const btn = document.querySelector("img.mobile-menu-button");
+  const menu = document.querySelector(".mobile-menu");
+
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
 };
