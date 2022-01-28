d5eb11247937ea4e43abee4bc7f082970522453c
bulldoguk <57550309+bulldoguk@users.noreply.github.com>
2022-01-27 21:26:25 -0600

    Basic articles animation with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
 module.exports = {
   content: ["./modules/**/*.{html,scss,js}", "./views/**/*.{html,scss,js}"],
   safelist: [
-    'bg-black',
-    'bg-white',
+    "bg-black",
+    "bg-white",
     {
       pattern: /bg-(white|black)/,
       pattern: /bg-(gray|green|blue)-(100|200|300|400|500)/,
     },
   ],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(4em) rotate(-12deg)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0) rotate(0deg)",
+          },
+        },
+      },
+    },
+    animation: {
+      "fade-in-up": "fade-in-up 1.0s ease-out",
+    },
+  },
   variants: {
     extend: {},
   },
