64da83c31edf3074e90ee16cd29486a15f74a26f
bulldoguk <57550309+bulldoguk@users.noreply.github.com>
2022-01-29 12:36:32 -0600

    Fixed mobile articles display

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   content: ["./modules/**/*.{html,scss,js}", "./views/**/*.{html,scss,js}"],
   safelist: [
-    "bg-black",
-    "bg-white",
-    {
-      pattern: /bg-(white|black)/,
-      pattern: /bg-(gray|green|blue)-(100|200|300|400|500)/,
-    },
   ],
   theme: {
     extend: {
